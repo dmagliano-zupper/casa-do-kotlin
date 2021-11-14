d867648d9e829e4ad439f25d7d2549c35f97738b
Diogo Magliano <diogo.magliano@zup.com.br>
2021-11-14 18:36:44 -0300

    Commit inicial

---
+++ src/main/kotlin/br/dev/magliano/casacodigokotlin/CasaCodigoKotlinApplication.kt
@@ -0,0 +1,11 @@
+package br.dev.magliano.casacodigokotlin
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CasaCodigoKotlinApplication
+
+fun main(args: Array<String>) {
+	runApplication<CasaCodigoKotlinApplication>(*args)
+}
---
+++ src/test/kotlin/br/dev/magliano/casacodigokotlin/CasaCodigoKotlinApplicationTests.kt
@@ -0,0 +1,13 @@
+package br.dev.magliano.casacodigokotlin
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CasaCodigoKotlinApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
